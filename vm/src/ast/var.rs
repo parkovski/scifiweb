07a95f461217d9ef4f938287cc7fb49d742f4e53
Parker Snell <parkersnell@gmail.com>
2017-09-13 20:38:49 -0700

    add scope

--- vm/src/ast/var.rs
+++ vm/src/ast/var.rs
@@ -1,5 +1,7 @@
 use std::sync::Arc;
+use fxhash::FxHashMap;
 use compile::{TokenSpan, TokenValue};
+use util::InsertGraphCell;
 use util::graph_cell::*;
 use super::*;
 use super::errors::*;
@@ -52,5 +54,59 @@ impl<'a> SourceItem for Variable<'a> {
 
   fn typecheck(&mut self) -> Result<()> {
     Ok(())
+  }
+}
+
+pub struct Scope<'a> {
+  vars: FxHashMap<Arc<str>, GraphCell<Variable<'a>>>,
+  parent: Option<GraphRefMut<'a, Scope<'a>>>,
+}
+
+impl<'a> Scope<'a> {
+  pub fn global() -> GraphCell<Self> {
+    GraphCell::new(Scope {
+      vars: Default::default(),
+      parent: None,
+    })
+  }
+
+  pub fn child(this: GraphRefMut<'a, Scope<'a>>) -> GraphCell<Self> {
+    GraphCell::new(Scope {
+      vars: Default::default(),
+      parent: Some(this),
+    })
+  }
+
+  pub fn end(&self, current: &mut GraphRefMut<'a, Scope<'a>>) -> Result<()> {
+    if let Some(parent) = self.parent {
+      *current = parent;
+      Ok(())
+    } else {
+      Err(ErrorKind::InvalidOperation("can't end the global scope").into())
+    }
+  }
+
+  pub fn contains(&self, name: &str) -> bool {
+    self.vars.contains_key(name) || self.parent.map_or(false, |p| p.awake().contains(name))
+  }
+
+  pub fn insert_var(&mut self, var: Variable<'a>) -> Result<GraphRefMut<'a, Variable<'a>>> {
+    self.vars.insert_graph_cell(var.source_name().value().clone(), var)
+      .map_err(|var| ErrorKind::DuplicateDefinition(
+          var.source_name().value().clone(), "variable"
+      ).into())
+  }
+}
+
+impl<'a> Owner<'a, Variable<'a>> for Scope<'a> {
+  fn find_mut(&self, name: &str) -> Option<GraphRefMut<'a, Variable<'a>>> {
+    self.vars.get(name)
+      .map(|v| v.asleep_mut())
+      .or_else(||
+        self.parent.map(|p|
+          p.awake().find_mut(name)
+        )
+        .unwrap_or(None)
+      )
   }
 }
